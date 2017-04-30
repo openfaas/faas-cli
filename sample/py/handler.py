304314a88e5dddba27a775b5a67d4b8309f12119
Alex <alexellis2@gmail.com>
2017-04-30 18:33:19 +0100

    Update Python example to optionally ping a URL

--- sample/py/handler.py
+++ sample/py/handler.py
@@ -1,2 +1,13 @@
-def handle(st):
-    print("Handle this -> " + st)
+import requests
+
+def print_url(url):
+    r =  requests.get(url)
+    print(url +" => " + str(r.status_code))
+
+def handle(req):
+    print("Handle this -> " + req)
+    if req.find("http") == -1:
+        print("Give me a URL and I'll ping it for you.")
+        return
+    
+    print_url(req)
--- test_python.sh
+++ test_python.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 docker service rm hello-py
-go build && ./faas-cli -action=build -image=alexellis2/hello-py -name=hello-py -handler=./sample/py -lang=python && \
+go build && \
+./faas-cli -action=build -image=alexellis2/hello-py -name=hello-py -handler=./sample/py -lang=python && \
 ./faas-cli -action=deploy -image=alexellis2/hello-py -name=hello-py -lang=python
-sleep 3
-curl -d "hi" http://localhost:8080/function/hello-py
 
+sleep 5
+
+curl -d "Hi" http://localhost:8080/function/hello-py
+curl -d "http://docs.get-faas.com" http://localhost:8080/function/hello-py
