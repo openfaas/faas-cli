71eb9070c1fad57d30127fae41a3397927331435
Alex Ellis <alexellis2@gmail.com>
2017-05-07 10:15:09 +0100

    Support YAML (#5)
    * YAML support experimental
    * Add optional fprocess
    * template function folder through Go's IO/API
    * Support env-vars
    * Document YAML

--- app.go
+++ app.go
@@ -11,6 +11,8 @@ import (
 	"os/exec"
 	"strings"
 
+	yaml "gopkg.in/yaml.v2"
+
 	"net/http"
 
 	"encoding/json"
@@ -33,6 +35,7 @@ func main() {
 	var language string
 	var replace bool
 	var nocache bool
+	var yamlFile string
 
 	flag.StringVar(&handler, "handler", "", "handler for function, i.e. handler.js")
 	flag.StringVar(&image, "image", "", "Docker image name to build")
@@ -44,7 +47,29 @@ func main() {
 	flag.BoolVar(&replace, "replace", true, "replace any existing function")
 	flag.BoolVar(&nocache, "no-cache", false, "do not use Docker's build cache")
 
+	flag.StringVar(&yamlFile, "yaml", "", "use a yaml file for a set of functions")
+
 	flag.Parse()
+
+	var services Services
+	if len(yamlFile) > 0 {
+		fileData, err := ioutil.ReadFile(yamlFile)
+		if err != nil {
+			fmt.Printf("Error: %s\n", err.Error())
+			return
+		}
+		err = yaml.Unmarshal(fileData, &services)
+		if err != nil {
+			fmt.Printf("Error with YAML file: %s\n", err.Error())
+			return
+		}
+
+		if services.Provider.Name != "faas" {
+			fmt.Println("'faas' is the only valid provider for the faas-cli.")
+			return
+		}
+
+	}
 
 	if len(action) == 0 {
 		fmt.Println("give either -action= build or deploy")
@@ -52,84 +77,87 @@ func main() {
 	}
 
 	if action == "build" {
-		if len(image) == 0 {
-			fmt.Println("Give a valid -image name for your Docker image.")
-			return
-		}
-		if len(handler) == 0 {
-			fmt.Println("Please give the full path to your function's handler.")
-			return
-		}
-		if len(functionName) == 0 {
-			fmt.Println("Please give the deployed -name of your function")
-			return
-		}
-
-		switch language {
-		case "node", "python":
-			tempPath := createBuildTemplate(functionName, handler, language)
-
-			fmt.Printf("Building: %s with Docker. Please wait..\n", image)
-
-			cacheFlag := ""
-			if nocache {
-				cacheFlag = " --no-cache"
+		if len(services.Functions) > 0 {
+			for k, function := range services.Functions {
+				function.Name = k
+				// fmt.Println(k, function)
+				fmt.Printf("Building: %s.\n", function.Name)
+				buildImage(function.Image, function.Handler, function.Name, function.Language, nocache)
 			}
-
-			builder := strings.Split(fmt.Sprintf("docker build %s-t %s .", cacheFlag, image), " ")
-			if len(os.Getenv("http_proxy")) > 0 || len(os.Getenv("http_proxy")) > 0 {
-				builder = strings.Split(fmt.Sprintf("docker build %s--build-arg http_proxy=%s --build-arg https_proxy=%s -t %s .", cacheFlag, os.Getenv("http_proxy"), os.Getenv("https_proxy"), image), " ")
+		} else {
+			if len(image) == 0 {
+				fmt.Println("Give a valid -image name for your Docker image.")
+				return
 			}
-
-			fmt.Println(strings.Join(builder, " "))
-			execBuild(tempPath, builder)
-		default:
-			log.Fatalf("Language template: %s not supported. Build a custom Dockerfile instead.", language)
+			if len(handler) == 0 {
+				fmt.Println("Please give the full path to your function's handler.")
+				return
+			}
+			if len(functionName) == 0 {
+				fmt.Println("Please give the deployed -name of your function")
+				return
+			}
+			buildImage(image, handler, functionName, language, nocache)
 		}
-
-		fmt.Printf("Image: %s built.\n", image)
 	} else if action == "deploy" {
-		if len(image) == 0 {
-			fmt.Println("Give an image name to be deployed.")
-			return
-		}
-		if len(functionName) == 0 {
-			fmt.Println("Give a -name for your function as it will be deployed on FaaS")
-			return
-		}
+		if len(services.Functions) > 0 {
+			for k, function := range services.Functions {
+				function.Name = k
+				// fmt.Println(k, function)
+				fmt.Printf("Deploying: %s.\n", function.Name)
 
-		// Need to alter Gateway to allow nil/empty string as fprocess, to avoid this repetition.
-		fprocessTemplate := "node index.js"
-		if len(fprocess) > 0 {
-			fprocessTemplate = fprocess
+				deployFunction(function.FProcess, services.Provider.GatewayURL, function.Name, function.Image, function.Language, replace, function.Environment)
+			}
+		} else {
+			if len(image) == 0 {
+				fmt.Println("Give an image name to be deployed.")
+				return
+			}
+			if len(functionName) == 0 {
+				fmt.Println("Give a -name for your function as it will be deployed on FaaS")
+				return
+			}
+			var envs map[string]string
+			deployFunction(fprocess, gateway, functionName, image, language, replace, envs)
 		}
-		if language == "python" {
-			fprocessTemplate = "python index.py"
-		}
-
-		if replace {
-			deleteFunction(gateway, functionName)
-		}
-
-		req := requests.CreateFunctionRequest{
-			EnvProcess: fprocessTemplate,
-			Image:      image,
-			Network:    "func_functions",
-			Service:    functionName,
-		}
-
-		reqBytes, _ := json.Marshal(&req)
-		reader := bytes.NewReader(reqBytes)
-		res, err := http.Post(gateway+"/system/functions", "application/json", reader)
-		if err != nil {
-			fmt.Println("Is FaaS deployed? Do you need to specify the -gateway flag?")
-			fmt.Println(err)
-			return
-		}
-		fmt.Println(res.Status)
-		deployedUrl := fmt.Sprintf("URL: %s/function/%s\n", gateway, functionName)
-		fmt.Println(deployedUrl)
 	}
+}
+
+func deployFunction(fprocess string, gateway string, functionName string, image string, language string, replace bool, envVars map[string]string) {
+
+	// Need to alter Gateway to allow nil/empty string as fprocess, to avoid this repetition.
+	fprocessTemplate := "node index.js"
+	if len(fprocess) > 0 {
+		fprocessTemplate = fprocess
+	}
+	if language == "python" {
+		fprocessTemplate = "python index.py"
+	}
+
+	if replace {
+		deleteFunction(gateway, functionName)
+	}
+
+	req := requests.CreateFunctionRequest{
+		EnvProcess: fprocessTemplate,
+		Image:      image,
+		Network:    "func_functions",
+		Service:    functionName,
+		EnvVars:    envVars,
+	}
+
+	reqBytes, _ := json.Marshal(&req)
+	reader := bytes.NewReader(reqBytes)
+	res, err := http.Post(gateway+"/system/functions", "application/json", reader)
+	if err != nil {
+		fmt.Println("Is FaaS deployed? Do you need to specify the -gateway flag?")
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(res.Status)
+	deployedURL := fmt.Sprintf("URL: %s/function/%s\n", gateway, functionName)
+	fmt.Println(deployedURL)
 }
 
 func deleteFunction(gateway string, functionName string) {
@@ -153,6 +181,33 @@ func deleteFunction(gateway string, functionName string) {
 	}
 }
 
+func buildImage(image string, handler string, functionName string, language string, nocache bool) {
+
+	switch language {
+	case "node", "python":
+		tempPath := createBuildTemplate(functionName, handler, language)
+
+		fmt.Printf("Building: %s with Docker. Please wait..\n", image)
+
+		cacheFlag := ""
+		if nocache {
+			cacheFlag = " --no-cache"
+		}
+
+		builder := strings.Split(fmt.Sprintf("docker build %s-t %s .", cacheFlag, image), " ")
+		if len(os.Getenv("http_proxy")) > 0 || len(os.Getenv("http_proxy")) > 0 {
+			builder = strings.Split(fmt.Sprintf("docker build %s--build-arg http_proxy=%s --build-arg https_proxy=%s -t %s .", cacheFlag, os.Getenv("http_proxy"), os.Getenv("https_proxy"), image), " ")
+		}
+
+		fmt.Println(strings.Join(builder, " "))
+		execBuild(tempPath, builder)
+	default:
+		log.Fatalf("Language template: %s not supported. Build a custom Dockerfile instead.", language)
+	}
+
+	fmt.Printf("Image: %s built.\n", image)
+}
+
 // createBuildTemplate creates temporary build folder to perform a Docker build with Node template
 func createBuildTemplate(functionName string, handler string, language string) string {
 	tempPath := fmt.Sprintf("./build/%s/", functionName)
@@ -171,8 +226,10 @@ func createBuildTemplate(functionName string, handler string, language string) s
 		fmt.Printf("Error creating path %s - %s.\n", functionPath, mkdirErr.Error())
 	}
 
+	// TODO: index folders and copy everything from template, rather than set folders.
 	// Drop in template
 	copyFiles("./template/"+language, tempPath)
+	copyFiles("./template/"+language+"/function", tempPath+"function/")
 
 	// Overlay in user-function
 	copyFiles(handler, tempPath+"function/")
