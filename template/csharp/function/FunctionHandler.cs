992aa9d08eac74163dd79153a0511d5c92ab50ee
Alex <alexellis2@gmail.com>
2017-08-16 17:30:47 +0100

    Enable CSharp template with .NET 2.0 standard
    Signed-off-by: Alex <alexellis2@gmail.com>

---
+++ template/csharp/function/FunctionHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Text;
+
+namespace Function
+{
+    public class FunctionHandler
+    {
+        public void Handle(string input) {
+            Console.WriteLine("Hi there - your input was: "+ input);
+        }
+    }
+}
