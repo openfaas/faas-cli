bdd7591720d1fdd146eb3b8e2e6d9d5e1bf4a59f
Alex Ellis <alexellis2@gmail.com>
2017-08-27 22:26:31 +0100

    Merge header notice from MIT
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- template/ruby/index.rb
+++ template/ruby/index.rb
@@ -1,3 +1,6 @@
+# Copyright (c) Alex Ellis 2017. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 require_relative 'function/handler'
 
 req = ARGF.read
