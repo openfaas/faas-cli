b5b308b766fe77ff16411db4f7318e206d8f0d46
John McCabe <john@johnmccabe.net>
2017-09-03 10:23:16 +0100

    Test version output during CI
    This commit adds a test script to check that the output of the `version`
    subcommand returns the GitSHA for the local workspace.
    
    This is added to the Travis build for inclusion in CI.
    
    Signed-off-by: John McCabe <john@johnmccabe.net>

---
+++ test/version.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+
+FAAS_BINARY=$1
+GIT_COMMIT=$(git rev-list -1 HEAD)
+
+echo "TEST: Version command prints GitCommit"
+${FAAS_BINARY} version | tee /dev/stderr | grep ${GIT_COMMIT}
+if [ $? -eq 0 ]; then
+    echo OK
+else
+    echo FAIL
+    exit 1
+fi
