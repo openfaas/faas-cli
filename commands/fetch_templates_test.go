b0e9b917b3467c81b641caad63f15018e14157bb
Eric Stoekl <ems5311@gmail.com>
2017-09-06 23:12:34 -0700

    Adds 'faas-cli template pull' command
    Pull language templates from any repo that has a 'template/' directory
    in the root with 'faas-cli template pull <github repo>'.
    
    Also allows 'faas-cli new --lang' to list available languages
    
    Signed-off-by: Eric Stoekl <ems5311@gmail.com>
    Signed-off-by: Minh-Quan TRAN <account@itscaro.me>

--- commands/fetch_templates_test.go
+++ commands/fetch_templates_test.go
@@ -9,16 +9,46 @@ import (
 	"testing"
 )
 
-var SmallestZipFile = []byte{80, 75, 05, 06, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00}
-
 func Test_PullTemplates(t *testing.T) {
-	// Remove existing master.zip file if it exists
-	if _, err := os.Stat("master.zip"); err == nil {
-		t.Log("Found a master.zip file, removing it...")
+	defer tearDown_fetch_templates(t)
 
-		err := os.Remove("master.zip")
+	// Create fake server for testing.
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "testdata/master_test.zip")
+	}))
+	defer ts.Close()
+
+	err := PullTemplates(ts.URL)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func Test_fetchTemplates(t *testing.T) {
+	defer tearDown_fetch_templates(t)
+
+	// Create fake server for testing.
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "testdata/master_test.zip")
+	}))
+	defer ts.Close()
+
+	err := fetchTemplates(ts.URL+"/owner/repo", false)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+// tearDown_fetch_templates_test cleans all files and directories created by the test
+func tearDown_fetch_templates(t *testing.T) {
+
+	// Remove existing archive file if it exists
+	if _, err := os.Stat("template-owner-repo.zip"); err == nil {
+		t.Log("The archive was not deleted")
+
+		err := os.Remove("template-owner-repo.zip")
 		if err != nil {
-			t.Fatal(err)
+			t.Log(err)
 		}
 	}
 
@@ -28,21 +58,15 @@ func Test_PullTemplates(t *testing.T) {
 
 		err := os.RemoveAll("template/")
 		if err != nil {
-			t.Fatal(err)
+			t.Log(err)
 		}
+	} else {
+		t.Logf("Directory template was not created: %s", err)
 	}
 
-	// Create fake server for testing.
-	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
-		// Write out the minimum number of bytes to make the response a valid .zip file
-		w.Write(SmallestZipFile)
-
-	}))
-	defer ts.Close()
-
-	err := PullTemplates(ts.URL)
-	if err != nil {
-		t.Error(err)
+	// Verify the downloaded archive
+	archive := "template-owner-repo.zip"
+	if _, err := os.Stat(archive); err == nil {
+		t.Fatalf("The archive %s was not deleted", archive)
 	}
 }
