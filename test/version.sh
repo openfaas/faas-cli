ac206c54c014b4288acc579b15c2b882ed56f5c4
Minh-Quan TRAN <account@itscaro.me>
2017-09-14 22:54:28 +0200

    Add tag into version command and shorten SHA1
    Signed-off-by: Minh-Quan TRAN <account@itscaro.me>

--- test/version.sh
+++ test/version.sh
@@ -1,10 +1,10 @@
-#!/bin/bash -x
+#!/bin/bash
 
 FAAS_BINARY=$1
 GIT_COMMIT=$(git rev-list -1 HEAD)
 
 echo "TEST: Version command prints GitCommit"
-${FAAS_BINARY} version | tee /dev/stderr | grep ${GIT_COMMIT}
+${FAAS_BINARY} version | tee /dev/stderr | grep "dev-${GIT_COMMIT:0:7}"
 if [ $? -eq 0 ]; then
     echo OK
 else
