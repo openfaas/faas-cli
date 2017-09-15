fb8924ef8f9339373a5f784565c9238ec54ebda0
Minh-Quan TRAN <account@itscaro.me>
2017-09-15 22:40:16 +0200

    Commit & Version in two separate lines
    Signed-off-by: Minh-Quan TRAN <account@itscaro.me>

--- test/version.sh
+++ test/version.sh
@@ -2,9 +2,12 @@
 
 FAAS_BINARY=$1
 GIT_COMMIT=$(git rev-list -1 HEAD)
+VERSION=$(git describe --all --exact-match `git rev-parse HEAD` | grep tags | sed 's/tags\///')
+VERSION=${VERSION:=dev}
 
 echo "TEST: Version command prints GitCommit"
-${FAAS_BINARY} version | tee /dev/stderr | grep "dev-${GIT_COMMIT:0:7}"
+${FAAS_BINARY} version | tee /dev/stderr | grep "Commit: ${GIT_COMMIT}"
+${FAAS_BINARY} version | tee /dev/stderr | grep "Version: ${VERSION}"
 if [ $? -eq 0 ]; then
     echo OK
 else
