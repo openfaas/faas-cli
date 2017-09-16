342eb539f27e7665a2fd69df9a62951095b601e2
Minh-Quan TRAN <account@itscaro.me>
2017-09-17 00:43:26 +0200

    add test for version command
    Signed-off-by: Minh-Quan TRAN <account@itscaro.me>

--- test/version.sh
+++ test/version.sh
@@ -2,12 +2,9 @@
 
 FAAS_BINARY=$1
 GIT_COMMIT=$(git rev-list -1 HEAD)
-VERSION=$(git describe --all --exact-match `git rev-parse HEAD` | grep tags | sed 's/tags\///')
-VERSION=${VERSION:=dev}
 
 echo "TEST: Version command prints GitCommit"
 ${FAAS_BINARY} version | tee /dev/stderr | grep "Commit: ${GIT_COMMIT}"
-${FAAS_BINARY} version | tee /dev/stderr | grep "Version: ${VERSION}"
 if [ $? -eq 0 ]; then
     echo OK
 else
