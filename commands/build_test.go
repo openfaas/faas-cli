8b12765100b54c2aed1c661793897dfd38962c39
Nenad Ilic <nenadilic84@gmail.com>
2017-10-30 21:41:35 +0100

    Added build_test
    Signed-off-by: Nenad Ilic <nenadilic84@gmail.com>

---
+++ commands/build_test.go
@@ -0,0 +1,25 @@
+// Copyright (c) Nenad Ilic 2017. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+package commands
+
+import (
+	"testing"
+)
+
+func Test_build(t *testing.T) {
+
+	aTests := [][]string{
+		{"build"},
+		{"build", "--image=my_image"},
+		{"build", "--image=my_image", "--handler=/path/to/fn/"},
+	}
+
+	for _, aTest := range aTests {
+		faasCmd.SetArgs(aTest)
+		err := faasCmd.Execute()
+		if err == nil {
+			t.Fatalf("No error found while testing \n%v", err)
+		}
+	}
+}
