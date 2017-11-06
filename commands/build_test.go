573ce563dcb80f85c8023107af8ced06d5aeb577
Nenad Ilic <nenadilic84@gmail.com>
2017-11-06 21:04:36 +0100

    build_test: Update the Copyright
    Signed-off-by: Nenad Ilic <nenadilic84@gmail.com>

--- commands/build_test.go
+++ commands/build_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) Nenad Ilic 2017. All rights reserved.
+// Copyright (c) OpenFaaS Project 2017. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 package commands
