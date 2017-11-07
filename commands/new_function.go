6abf8a9954c94fc47fc6d3ce690cfeda49422f86
Eric Stoekl <ems5311@gmail.com>
2017-11-06 22:18:42 -0800

    template pull - address issues from thread #201
    1) Moves code that merges zip file contents into template/ dir into
    expandTemplatesFromZip() function. Removes hard-coded constant.
    2) Sort list of languages found with 'faas-cli new --list' before
    printing
    3) Fixes for typos, more descriptive comments, and remove incorrect
    license attribution
    
    Signed-off-by: Eric Stoekl <ems5311@gmail.com>

--- commands/new_function.go
+++ commands/new_function.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"sort"
 	"strings"
 
 	"github.com/morikuni/aec"
@@ -19,6 +20,13 @@ var (
 	lang string
 	list bool
 )
+
+// Implement interface for sorting array of strings
+type StrSort []string
+
+func (a StrSort) Len() int           { return len(a) }
+func (a StrSort) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a StrSort) Less(i, j int) bool { return a[i] < a[j] }
 
 func init() {
 	newFunctionCmd.Flags().StringVar(&lang, "lang", "", "Language or template to use")
@@ -145,6 +153,7 @@ functions:
 
 func printAvailableTemplates(availableTemplates []string) string {
 	var result string
+	sort.Sort(StrSort(availableTemplates))
 	for _, template := range availableTemplates {
 		result += fmt.Sprintf("- %s\n", template)
 	}
