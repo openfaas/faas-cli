5e03417620ad39347536da9b4f0d111e526b05d4
Minh-Quan TRAN <account@itscaro.me>
2018-01-24 02:09:13 +0100

    build: add method to create command instead of instantiating command directly in init()
    Signed-off-by: Minh-Quan TRAN <account@itscaro.me>

--- commands/build_test.go
+++ commands/build_test.go
@@ -10,14 +10,15 @@ import (
 func Test_build(t *testing.T) {
 
 	aTests := [][]string{
-		{"build"},
-		{"build", "--image=my_image"},
-		{"build", "--image=my_image", "--handler=/path/to/fn/"},
+		{},
+		{"--image=my_image"},
+		{"--image=my_image", "--handler=/path/to/fn/"},
 	}
 
 	for _, aTest := range aTests {
-		faasCmd.SetArgs(aTest)
-		err := faasCmd.Execute()
+		buildCmd := newBuildCmd()
+		buildCmd.SetArgs(aTest)
+		err := buildCmd.Execute()
 		if err == nil {
 			t.Fatalf("No error found while testing \n%v", err)
 		}
