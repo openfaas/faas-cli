5856c8896393a02fc763f7524cbef1a1eb750c83
Minh-Quan TRAN <account@itscaro.me>
2018-01-31 22:42:11 +0100

    re-factor `store` commands
    copyright year update for new files
    
    Signed-off-by: Minh-Quan TRAN <account@itscaro.me>

---
+++ commands/store_list.go
@@ -0,0 +1,41 @@
+// Copyright (c) OpenFaaS Project 2018. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+package commands
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	// Setup flags used by store command
+	storeListCmd.Flags().BoolVarP(&verboseDescription, "verbose", "v", false, "Verbose output for the field values")
+
+	storeCmd.AddCommand(storeListCmd)
+}
+
+var storeListCmd = &cobra.Command{
+	Use:     `list [--url STORE_URL]`,
+	Short:   "List OpenFaaS store items",
+	Long:    "Lists the available items in OpenFaas store",
+	Example: `  faas-cli store list --url https://domain:port/store.json`,
+	RunE:    runStoreList,
+}
+
+func runStoreList(cmd *cobra.Command, args []string) error {
+	items, err := storeList(storeAddress)
+	if err != nil {
+		return err
+	}
+
+	if len(items) == 0 {
+		fmt.Printf("The store is empty.")
+		return nil
+	}
+
+	fmt.Print(renderStoreItems(items))
+
+	return nil
+}
