e81362363338afde4ca84a4568f728ede7406fb7
Alex Ellis (VMware) <alexellis2@gmail.com>
2018-02-28 14:59:51 +0000

    Update license
    Signed-off-by: Alex Ellis (VMware) <alexellis2@gmail.com>

--- commands/push_test.go
+++ commands/push_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) OpenFaaS project 2018. All rights reserved.
+// Copyright (c) OpenFaaS Project 2018. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 package commands
