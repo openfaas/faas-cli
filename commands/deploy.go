b7c6f0bf79e871e8e74661c56520d5010df5ac5b
Alex Ellis (VMware) <alexellis2@gmail.com>
2018-05-15 13:29:03 +0100

    Fix issue via #410
    Issue #410 shows registryAuth sent for local Docker images which
    is not required. This change and test covers the scenario and
    means that no error will be shown for invalid registry auth
    passed when it was not required.
    
    Signed-off-by: Alex Ellis (VMware) <alexellis2@gmail.com>

--- commands/deploy.go
+++ commands/deploy.go
@@ -171,6 +171,7 @@ func runDeployCommand(args []string, image string, fprocess string, functionName
 				}
 
 				function.RegistryAuth = getRegistryAuth(&dockerConfig, function.Image)
+
 			}
 
 			fileEnvironment, err := readFiles(function.EnvironmentFile)
@@ -449,6 +450,11 @@ func readDockerConfig(config *configFile) error {
 }
 
 func getRegistryAuth(config *configFile, image string) string {
+
+	// The local library does not require an auth string.
+	if strings.Contains(image, "/") == false {
+		return ""
+	}
 
 	if len(config.AuthConfigs) > 0 {
 
