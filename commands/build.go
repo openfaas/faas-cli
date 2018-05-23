1909fd6d8fdfc300533cc1bb53231bd64c8c21f2
Ivana Yovcheva (VMware) <iyovcheva@vmware.com>
2018-05-23 20:10:39 +0300

    Add support for building native modules i.e. C/C++
    This change adds a `build-option` flag to `faas-cli build`, e.g.
    `faas-cli  build  -f  stack.yml --build-option=dev`. If  the
    function  language  template  has  applied  `build_options`
    specification in its `template.yml` file, i.e.
    ```
    build_options:
      - name: dev
        packages:
          - musl-dev
          - gcc
          - make
    ```
    then `ADDITIONAL_PACKAGE` agrument will be passed by `build-arg`.
    This allows the listed packages to be built by the template's
    `Dockerfile` if it is edited to support the feature.
    
    To see an example of how to edit a languge template to addopt the
    new feature, please review the python3 template.
    
    Signed-off-by: Ivana Yovcheva (VMware) <iyovcheva@vmware.com>

--- commands/build.go
+++ commands/build.go
@@ -24,7 +24,10 @@ var (
 	shrinkwrap  bool
 	buildArgs   []string
 	buildArgMap map[string]string
+	buildOption string
 )
+
+const additionalPackageBuildArg = "ADDITIONAL_PACKAGE"
 
 func init() {
 	// Setup flags that are used by multiple commands (variables defined in faas.go)
@@ -43,6 +46,8 @@ func init() {
 
 	buildCmd.Flags().StringArrayVarP(&buildArgs, "build-arg", "b", []string{}, "Add a build-arg for Docker (KEY=VALUE)")
 
+	buildCmd.Flags().StringVar(&buildOption, "build-option", "", "Set a build option, e.g. dev")
+
 	// Set bash-completion.
 	_ = buildCmd.Flags().SetAnnotation("handler", cobra.BashCompSubdirsInDir, []string{})
 
@@ -60,13 +65,15 @@ var buildCmd = &cobra.Command{
                  [--regex "REGEX"]
 				 [--filter "WILDCARD"]
 				 [--parallel PARALLEL_DEPTH]
-				 [--build-arg KEY=VALUE]`,
+				 [--build-arg KEY=VALUE]
+				 [--build-option VALUE]`,
 	Short: "Builds OpenFaaS function containers",
 	Long: `Builds OpenFaaS function containers either via the supplied YAML config using
 the "--yaml" flag (which may contain multiple function definitions), or directly
 via flags.`,
 	Example: `  faas-cli build -f https://domain/path/myfunctions.yml
   faas-cli build -f ./stack.yml --no-cache --build-arg NPM_VERSION=0.2.2
+  faas-cli build -f ./stack.yml --build-option dev
   faas-cli build -f ./stack.yml --filter "*gif*"
   faas-cli build -f ./stack.yml --regex "fn[0-9]_.*"
   faas-cli build --image=my_image --lang=python --handler=/path/to/fn/ 
@@ -78,6 +85,14 @@ via flags.`,
 // preRunBuild validates args & flags
 func preRunBuild(cmd *cobra.Command, args []string) error {
 	language, _ = validateLanguageFlag(language)
+
+	if buildOption != "" {
+		arg, validBuildOption, err := validateBuildOption(buildOption, language)
+
+		if validBuildOption && err == nil {
+			buildArgs = append(buildArgs, arg)
+		}
+	}
 
 	mapped, err := parseBuildArgs(buildArgs)
 
@@ -109,10 +124,70 @@ func parseBuildArgs(args []string) (map[string]string, error) {
 			return nil, fmt.Errorf("build-arg must have a non-empty value")
 		}
 
-		mapped[k] = v
+		if k == additionalPackageBuildArg && len(mapped[k]) > 0 {
+			mapped[k] = mapped[k] + " " + v
+		} else {
+			mapped[k] = v
+		}
 	}
 
 	return mapped, nil
+}
+
+func validateBuildOption(buildOption string, language string) (string, bool, error) {
+
+	buildOptions, err := deriveBuildOptions(language)
+
+	if err != nil {
+		return "", false, err
+	}
+
+	foundOption, isFound := findBuildOption(buildOptions, buildOption)
+
+	if isFound {
+		buildOptionPackages := strings.Join(foundOption.Packages, " ")
+		return additionalPackageBuildArg + "=" + buildOptionPackages, isFound, err
+	}
+
+	err = fmt.Errorf("ERROR! You're using a wrong build option. Please check the  template/language/template.yml for supported build options.")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return "", false, err
+}
+
+func deriveBuildOptions(language string) ([]stack.BuildOption, error) {
+	var buildOptions = []stack.BuildOption{}
+
+	pathToTemplateYAML := "./template/" + language + "/template.yml"
+	if _, err := os.Stat(pathToTemplateYAML); os.IsNotExist(err) {
+		return buildOptions, err
+	}
+
+	var langTemplate stack.LanguageTemplate
+	parsedLangTemplate, err := stack.ParseYAMLForLanguageTemplate(pathToTemplateYAML)
+
+	if err != nil {
+		return buildOptions, err
+	}
+
+	if parsedLangTemplate != nil {
+		langTemplate = *parsedLangTemplate
+		buildOptions = langTemplate.BuildOptions
+	}
+
+	return buildOptions, nil
+}
+
+func findBuildOption(buildOptions []stack.BuildOption, requiredBuildOption string) (stack.BuildOption, bool) {
+	for _, option := range buildOptions {
+		if option.Name == requiredBuildOption {
+			return option, true
+		}
+	}
+
+	return stack.BuildOption{}, false
 }
 
 func runBuild(cmd *cobra.Command, args []string) error {
--- commands/build_test.go
+++ commands/build_test.go
@@ -4,6 +4,7 @@
 package commands
 
 import (
+	"os"
 	"testing"
 )
 
@@ -73,4 +74,78 @@ func Test_parseBuildArgs_MultipleSeparators(t *testing.T) {
 		t.Errorf("value for 'k', want: %s got: %s", "v=z", mapped["k"])
 		t.Fail()
 	}
+}
+
+func Test_validateBuildOption(t *testing.T) {
+
+	buildOptions := []struct {
+		buildOption           string
+		expectedBuildArgValue string
+	}{
+		{
+			buildOption:           "dev",
+			expectedBuildArgValue: "ADDITIONAL_PACKAGE=make automake gcc g++ subversion python3-dev musl-dev libffi-dev",
+		},
+
+		{
+			buildOption:           "undefined",
+			expectedBuildArgValue: "",
+		},
+	}
+
+	os.MkdirAll("template/python3", os.ModePerm)
+	python3_template_yml, err := os.Create("template/python3/template.yml")
+	if err != nil {
+		t.Errorf("Error creating template/python3/template.yml file")
+	}
+
+	_, err = python3_template_yml.WriteString("language: python3\n" +
+		"fprocess: python3 index.py\n" +
+		"build_options: \n" +
+		"  - name: dev\n" +
+		"    packages: \n" +
+		"      - make\n" +
+		"      - automake\n" +
+		"      - gcc\n" +
+		"      - g++\n" +
+		"      - subversion\n" +
+		"      - python3-dev\n" +
+		"      - musl-dev\n" +
+		"      - libffi-dev\n")
+
+	if err != nil {
+		t.Errorf("Error writing to template/python3/template.yml file")
+	}
+
+	os.MkdirAll("template/unsupported", os.ModePerm)
+	unsupported_template_yml, err := os.Create("template/unsupported/template.yml")
+	if err != nil {
+		t.Errorf("Error creating template/unsupported/template.yml file")
+	}
+
+	_, err = unsupported_template_yml.WriteString("language: python3\n" +
+		"fprocess: python3 index.py\n")
+
+	if err != nil {
+		t.Errorf("Error writing to template/pythunsupportedon3/template.yml file")
+	}
+
+	for _, test := range buildOptions {
+		t.Run(test.buildOption, func(t *testing.T) {
+			res, _, _ := validateBuildOption(test.buildOption, "python3")
+			_, isValid, _ := validateBuildOption(test.buildOption, "unsupported")
+
+			if res != test.expectedBuildArgValue {
+				t.Errorf("validateBuildOption failed for build-option %s. Expected to return %s, but returned %s",
+					test.buildOption, test.expectedBuildArgValue, res)
+			}
+
+			if isValid && test.buildOption == "dev" {
+				t.Errorf("validateBuildOption failed for build-option %s and unsupported language. Expected validation to fail, but it was successful",
+					test.buildOption)
+			}
+		})
+	}
+
+	os.RemoveAll("template")
 }
