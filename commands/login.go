8ded58db45e83219347b8ba7a719b85bcbd7830e
Alex Ellis (VMware) <alexellis2@gmail.com>
2018-07-07 22:15:40 +0100

    Fix login command to read gw env-var
    Every command can determine the gateway from a number of sources
    including arguments, YAML and the env-var OPENFAAS_URL. A small
    typo/bug in the login command meant the env-var was never
    honoured. This change is tested with remote login and with the
    unit tests with Swarm/MacOS.
    
    Signed-off-by: Alex Ellis (VMware) <alexellis2@gmail.com>

--- commands/login.go
+++ commands/login.go
@@ -77,7 +77,7 @@ func runLogin(cmd *cobra.Command, args []string) error {
 
 	fmt.Println("Calling the OpenFaaS server to validate the credentials...")
 
-	gateway = getGatewayURL(gateway, "", "", os.Getenv(openFaaSURLEnvironment))
+	gateway = getGatewayURL(gateway, defaultGateway, "", os.Getenv(openFaaSURLEnvironment))
 
 	if err := validateLogin(gateway, username, password); err != nil {
 		return err
