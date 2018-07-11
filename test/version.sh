10e41c1840c29423fb6bb2ea23f30f639781d3fc
Edward Wilde <ewilde@gmail.com>
2018-07-11 22:20:22 +0100

    PR feedback for version info command update
    1. Increase timeout to 5s
    2. Defer closing response body
    3. Use back tick for string formatting
    4. Refactor unit tests to use test tables
    5. Indent version command output with space
    6. Favor http.NewRequest/Do over Client.Get for consistency
    
    Signed-off-by: Edward Wilde <ewilde@gmail.com>

--- test/version.sh
+++ test/version.sh
@@ -4,7 +4,7 @@ FAAS_BINARY=$1
 GIT_COMMIT=$(git rev-list -1 HEAD)
 
 echo "TEST: Version command prints GitCommit"
-${FAAS_BINARY} version | tee /dev/stderr | grep "CLI commit: ${GIT_COMMIT}"
+${FAAS_BINARY} version | tee /dev/stderr | grep "commit:  ${GIT_COMMIT}"
 if [ $? -eq 0 ]; then
     echo OK
 else
