7b36bb38e5eee0ef6eed1bb26b660fb2e8624cec
Vivek Singh <vivekkmr45@yahoo.in>
2018-08-02 22:56:03 +0530

    Allow blank value for namespace
    This commit add changes to allow blank value for namespace, which
    removes namespace field from CRD definition file.
    
    Also updated year for the copyright.
    
    Signed-off-by: Vivek Singh <vivekkmr45@yahoo.in>

--- commands/generate.go
+++ commands/generate.go
@@ -1,4 +1,4 @@
-// Copyright (c) OpenFaaS Author(s) 2017. All rights reserved.
+// Copyright (c) OpenFaaS Author(s) 2018. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 package commands
