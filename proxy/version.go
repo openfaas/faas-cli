2271d71dbb0265a9ac93d2286099cf9a5a226784
Vivek Singh <vivekkmr45@yahoo.in>
2018-09-02 17:11:39 +0530

    Proxy version command HTTP call
    This commit proxies the HTTP calls made in `version` command.
    
    Fixes : #519
    
    Signed-off-by: Vivek Singh <vivekkmr45@yahoo.in>

---
+++ proxy/version.go
@@ -0,0 +1,56 @@
+// Copyright (c) OpenFaaS Author(s) 2018. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+package proxy
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+//GetSystemInfo get system information from /system/info endpoint
+func GetSystemInfo(gateway string, tlsInsecure bool) (map[string]interface{}, error) {
+	infoEndPoint := gateway + "/system/info"
+	timeout := 5 * time.Second
+
+	client := MakeHTTPClient(&timeout, tlsInsecure)
+	req, err := http.NewRequest("GET", infoEndPoint, nil)
+	if err != nil {
+		return nil, fmt.Errorf("cannot connect to OpenFaaS on URL: %s", gateway)
+	}
+
+	response, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("cannot connect to OpenFaaS on URL: %s", gateway)
+	}
+
+	if response.Body != nil {
+		defer response.Body.Close()
+	}
+	info := make(map[string]interface{})
+
+	switch response.StatusCode {
+	case http.StatusOK:
+		bytesOut, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			return nil, fmt.Errorf("cannot read result from OpenFaaS on URL: %s", gateway)
+		}
+		err = json.Unmarshal(bytesOut, &info)
+		if err != nil {
+			return nil, fmt.Errorf("cannot parse result from OpenFaaS on URL: %s\n%s", gateway, err.Error())
+		}
+
+	case http.StatusUnauthorized:
+		return nil, fmt.Errorf("unauthorized access, run \"faas-cli login\" to setup authentication for this server")
+	default:
+		bytesOut, err := ioutil.ReadAll(response.Body)
+		if err == nil {
+			return nil, fmt.Errorf("server returned unexpected status code: %d - %s", response.StatusCode, string(bytesOut))
+		}
+	}
+
+	return info, nil
+}
