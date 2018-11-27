5b36c50280d961d3aa248bd17f901f4ef2774447
Martin Dekov (VMware) <mdekov@vmware.com>
2018-11-27 06:04:35 -0800

    Remove multiple repos feature
    Removing multiple repositories pull feature for
    simplicity along with adding information on the
    command in TEMPLATE.md file
    
    Signed-off-by: Martin Dekov (VMware) <mdekov@vmware.com>

--- commands/template_store_pull.go
+++ commands/template_store_pull.go
@@ -6,7 +6,6 @@ package commands
 import (
 	"fmt"
 	"os"
-	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -24,18 +23,19 @@ var templateStorePullCmd = &cobra.Command{
 	Use:   `pull [TEMPLATE_NAME]`,
 	Short: `Pull templates from store`,
 	Long:  `Pull templates from store supported by openfaas or openfaas-incubator organizations or your custom store`,
-	Example: `  faas-cli template store pull ruby-http ...
-  faas-cli template store pull openfaas/go openfaas-incubator/node10-express ...
-  faas-cli template store pull go ... --debug
-  faas-cli template store pull openfaas/go openfaas-incubator/node10-express --overwrite
+	Example: `  faas-cli template store pull ruby-http
+  faas-cli template store pull go --debug
+  faas-cli template store pull openfaas/go --overwrite
   faas-cli template store pull golang-middleware --url https://raw.githubusercontent.com/openfaas/store/master/templates.json`,
 	RunE: runTemplateStorePull,
 }
 
 func runTemplateStorePull(cmd *cobra.Command, args []string) error {
-	var nonExistingTemplates []string
 	if len(args) == 0 {
-		return fmt.Errorf("\nNeed to specify one of the store templates check available ones by running the command:\n\nfaas-cli template store list\n")
+		return fmt.Errorf("\nNeed to specify one of the store templates, check available ones by running the command:\n\nfaas-cli template store list\n")
+	}
+	if len(args) > 1 {
+		return fmt.Errorf("\nNeed to specify single template from the store, check available ones by running the command:\n\nfaas-cli template store list\n")
 	}
 
 	envTemplateRepoStore := os.Getenv(templateStoreURLEnvironment)
@@ -46,27 +46,21 @@ func runTemplateStorePull(cmd *cobra.Command, args []string) error {
 		return fmt.Errorf("error while fetching templates from store: %s", templatesErr)
 	}
 
-	for _, template := range args {
-		found := false
-		for _, storeTemplate := range storeTemplates {
-			sourceName := fmt.Sprintf("%s/%s", storeTemplate.Source, storeTemplate.TemplateName)
-			if template == storeTemplate.TemplateName || template == sourceName {
-				err := runTemplatePull(cmd, []string{storeTemplate.Repository})
-				if err != nil {
-					return fmt.Errorf("error while pulling template: %s : %s", storeTemplate.TemplateName, err.Error())
-				}
-				found = true
-				break
+	templateName := args[0]
+	found := false
+	for _, storeTemplate := range storeTemplates {
+		sourceName := fmt.Sprintf("%s/%s", storeTemplate.Source, storeTemplate.TemplateName)
+		if templateName == storeTemplate.TemplateName || templateName == sourceName {
+			err := runTemplatePull(cmd, []string{storeTemplate.Repository})
+			if err != nil {
+				return fmt.Errorf("error while pulling template: %s : %s", storeTemplate.TemplateName, err.Error())
 			}
-		}
-		if !found {
-			nonExistingTemplates = append(nonExistingTemplates, fmt.Sprintf("\nThere is no template with name: `%s` in the store.\n", template))
+			found = true
+			break
 		}
 	}
-	if len(nonExistingTemplates) > 0 {
-		nonExistingTemplates = append(nonExistingTemplates, "\ncheck available options by running:\n\nfaas-cli template store pull <NAME>|<SOURCE/NAME>\n")
-		return fmt.Errorf("%s", strings.Join(nonExistingTemplates, ""))
+	if !found {
+		return fmt.Errorf("template with name: `%s` does not exist in the repo", templateName)
 	}
-
 	return nil
 }
