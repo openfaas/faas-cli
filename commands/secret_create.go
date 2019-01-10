56de501fcbb6737fed78df6919cfdc17bfde6648
Vivek Singh <vivekkmr45@yahoo.in>
2019-01-10 22:53:25 +0530

    Add STDIN support for secret input
    Signed-off-by: Vivek Singh <vivekkmr45@yahoo.in>

--- commands/secret_create.go
+++ commands/secret_create.go
@@ -1,4 +1,4 @@
-// Copyright (c) OpenFaaS Author(s) 2018. All rights reserved.
+// Copyright (c) OpenFaaS Author(s) 2019. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 package commands
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strings"
 
 	"github.com/openfaas/faas-cli/proxy"
 	"github.com/spf13/cobra"
@@ -20,61 +21,59 @@ var (
 
 // secretCreateCmd represents the secretCreate command
 var secretCreateCmd = &cobra.Command{
-	Use:   "create SECRET_NAME --value=SECRET_VALUE --from-file=/path/to/secret/file",
+	Use:   "create SECRET_NAME [--from-literal=SECRET_VALUE] [--from-file=/path/to/secret/file] [STDIN]",
 	Short: "Create a new secret",
-	Long:  `The create command creates a new secret`,
-	Example: `faas-cli secret create secret-name --value=secret-value
-faas-cli secret create secret-name --value=secret-value --gateway=http://127.0.0.1:8080
-faas-cli secret create secret-name --from-file=/path/to/secret/file --gateway=http://127.0.0.1:8080`,
+	Long:  `The create command creates a new secret from file, literal or STDIN`,
+	Example: `faas-cli secret create secret-name --from-literal=secret-value
+faas-cli secret create secret-name --from-literal=secret-value --gateway=http://127.0.0.1:8080
+faas-cli secret create secret-name --from-file=/path/to/secret/file --gateway=http://127.0.0.1:8080
+cat /path/to/secret/file | faas-cli secret create secret-name`,
 	RunE:    runSecretCreate,
 	PreRunE: preRunSecretCreate,
 }
 
 func init() {
-	secretCreateCmd.Flags().StringVar(&secretValue, "value", "", "Value of the secret")
+	secretCreateCmd.Flags().StringVar(&secretValue, "from-literal", "", "Value of the secret")
 	secretCreateCmd.Flags().StringVar(&secretFile, "from-file", "", "Path to the secret file")
 	secretCreateCmd.Flags().BoolVar(&tlsInsecure, "tls-no-verify", false, "Disable TLS validation")
 	secretCreateCmd.Flags().StringVarP(&gateway, "gateway", "g", defaultGateway, "Gateway URL starting with http(s)://")
 	secretCmd.AddCommand(secretCreateCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// secretCreateCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// secretCreateCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 func preRunSecretCreate(cmd *cobra.Command, args []string) error {
-
-	if len(secretValue) == 0 && len(secretFile) == 0 {
-		return fmt.Errorf("please provide a value or file path for the secret")
-	}
-
-	if len(secretValue) != 0 && len(secretFile) != 0 {
-		return fmt.Errorf("please either provide value or file for the secret")
-	}
 
 	return nil
 }
 
 func runSecretCreate(cmd *cobra.Command, args []string) error {
 	var gatewayAddress string
+	var err error
 
 	if len(args) < 1 {
 		return fmt.Errorf("please provide secret name")
 	}
 	secretName = args[0]
 
-	if len(secretFile) > 0 {
-		fileSecretValue, err := readSecretFromFile(secretFile)
+	if len(secretValue) == 0 && len(secretFile) > 0 {
+		secretValue, err = readSecretFromFile(secretFile)
 		if err != nil {
 			return err
 		}
-		secretValue = fileSecretValue
+
+	}
+
+	if len(secretValue) == 0 && len(secretFile) == 0 {
+		secretStdin, err := ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			return err
+		}
+
+		secretValue = string(secretStdin)
+	}
+	secretValue = strings.TrimSpace(secretValue)
+
+	if len(secretValue) == 0 {
+		return fmt.Errorf("must provide a non empty secret via --from-literal, --from-file or STDIN")
 	}
 
 	gatewayAddress = getGatewayURL(gateway, defaultGateway, "", os.Getenv(openFaaSURLEnvironment))
