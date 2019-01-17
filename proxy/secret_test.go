2487d1d3443c9b7201c98a2ecc57b617ac58db56
Vivek Singh <vivekkmr45@yahoo.in>
2019-01-17 19:57:00 +0530

    Add unit test cases for secrets proxy
    Signed-off-by: Vivek Singh <vivekkmr45@yahoo.in>

---
+++ proxy/secret_test.go
@@ -0,0 +1,171 @@
+package proxy
+
+import (
+	"net/http"
+	"regexp"
+	"testing"
+
+	"github.com/openfaas/faas-cli/schema"
+	"github.com/openfaas/faas-cli/test"
+)
+
+func Test_GetSecretList_200OK(t *testing.T) {
+	s := test.MockHttpServer(t, []test.Request{
+		{
+			ResponseStatusCode: http.StatusOK,
+			ResponseBody:       expectedSecretList,
+		},
+	})
+	defer s.Close()
+
+	secrets, err := GetSecretList(s.URL, true)
+	if err != nil {
+		t.Errorf("Error returned: %s", err.Error())
+	}
+
+	for k, v := range secrets {
+		if expectedSecretList[k] != v {
+			t.Fatalf("Expeceted: %#v - Actual: %#v", expectedListFunctionsResponse[k], v)
+		}
+	}
+}
+
+func Test_GetSecretList_202Accepted(t *testing.T) {
+	s := test.MockHttpServer(t, []test.Request{
+		{
+			ResponseStatusCode: http.StatusAccepted,
+			ResponseBody:       expectedSecretList,
+		},
+	})
+	defer s.Close()
+
+	secrets, err := GetSecretList(s.URL, true)
+	if err != nil {
+		t.Errorf("Error returned: %s", err.Error())
+	}
+
+	for k, v := range secrets {
+		if expectedSecretList[k] != v {
+			t.Fatalf("Expeceted: %#v - Actual: %#v", expectedListFunctionsResponse[k], v)
+		}
+	}
+}
+
+func Test_GetSecretList_Not200(t *testing.T) {
+	s := test.MockHttpServerStatus(t, http.StatusBadRequest)
+
+	_, err := GetSecretList(s.URL, true)
+
+	if err == nil {
+		t.Fatalf("Error was not returned")
+	}
+
+	r := regexp.MustCompile(`(?m:server returned unexpected status code)`)
+	if !r.MatchString(err.Error()) {
+		t.Fatalf("Error not matched: %s", err)
+	}
+}
+
+func Test_GetSecretList_Unauthorized401(t *testing.T) {
+	s := test.MockHttpServerStatus(t, http.StatusUnauthorized)
+
+	_, err := GetSecretList(s.URL, true)
+
+	if err == nil {
+		t.Fatalf("Error was not returned")
+	}
+
+	r := regexp.MustCompile(`(?m:unauthorized access, run \"faas-cli login\" to setup authentication for this server)`)
+	if !r.MatchString(err.Error()) {
+		t.Fatalf("Error not matched: %s", err)
+	}
+}
+
+var expectedSecretList = []schema.Secret{
+	{
+		Name: "Secret1",
+	},
+	{
+		Name: "Secret2",
+	},
+}
+
+func Test_CreateSecret_200OK(t *testing.T) {
+	s := test.MockHttpServerStatus(t, http.StatusOK)
+	secret := schema.Secret{
+		Name:  "secret-name",
+		Value: "secret-value",
+	}
+
+	status, _ := CreateSecret(s.URL, secret, true)
+
+	if status != http.StatusOK {
+		t.Errorf("expected: %d, got: %d", http.StatusOK, status)
+	}
+}
+
+func Test_CreateSecret_201Created(t *testing.T) {
+	s := test.MockHttpServerStatus(t, http.StatusCreated)
+	secret := schema.Secret{
+		Name:  "secret-name",
+		Value: "secret-value",
+	}
+
+	status, _ := CreateSecret(s.URL, secret, true)
+
+	if status != http.StatusCreated {
+		t.Errorf("expected: %d, got: %d", http.StatusCreated, status)
+	}
+}
+
+func Test_CreateSecret_202Accepted(t *testing.T) {
+	s := test.MockHttpServerStatus(t, http.StatusAccepted)
+	secret := schema.Secret{
+		Name:  "secret-name",
+		Value: "secret-value",
+	}
+
+	status, _ := CreateSecret(s.URL, secret, true)
+
+	if status != http.StatusAccepted {
+		t.Errorf("expected: %d, got: %d", http.StatusAccepted, status)
+	}
+}
+
+func Test_CreateSecret_Not200(t *testing.T) {
+	s := test.MockHttpServerStatus(t, http.StatusBadRequest)
+
+	secret := schema.Secret{
+		Name:  "secret-name",
+		Value: "secret-value",
+	}
+	status, output := CreateSecret(s.URL, secret, true)
+
+	if status != http.StatusBadRequest {
+		t.Errorf("expected: %d, got: %d", http.StatusBadRequest, status)
+	}
+
+	r := regexp.MustCompile(`(?m:server returned unexpected status code)`)
+	if !r.MatchString(output) {
+		t.Fatalf("Error not matched: %s", output)
+	}
+}
+
+func Test_CreateSecret_Unauthorized401(t *testing.T) {
+	s := test.MockHttpServerStatus(t, http.StatusUnauthorized)
+
+	secret := schema.Secret{
+		Name:  "secret-name",
+		Value: "secret-value",
+	}
+	status, output := CreateSecret(s.URL, secret, true)
+
+	if status != http.StatusUnauthorized {
+		t.Errorf("expected: %d, got: %d", http.StatusUnauthorized, status)
+	}
+
+	r := regexp.MustCompile(`(?m:unauthorized access, run \"faas-cli login\" to setup authentication for this server)`)
+	if !r.MatchString(output) {
+		t.Fatalf("Error not matched: %s", output)
+	}
+}
