44116bb93102462a276e521cde70fe84c1e0486e
Vivek Singh <vivekkmr45@yahoo.in>
2019-04-08 00:20:56 +0530

    Add unit test for filter store and review update
    Signed-off-by: Vivek Singh <vivekkmr45@yahoo.in>

---
+++ commands/store_test.go
@@ -0,0 +1,202 @@
+package commands
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/openfaas/faas-cli/schema"
+)
+
+func matchFilteredOutout(t *testing.T, expectedOuputFunctions, filteredFunctions []schema.StoreFunction, platform string) {
+	if len(expectedOuputFunctions) != len(filteredFunctions) {
+		t.Errorf("Length did not match, expected: %v, got: %v", len(expectedOuputFunctions), len(filteredFunctions))
+	}
+	var isFound bool
+
+	for _, expectedFunction := range expectedOuputFunctions {
+		isFound = false
+
+		for _, filteredFunction := range filteredFunctions {
+			_, hasPlatform := filteredFunction.Images[platform]
+			if expectedFunction.Name == filteredFunction.Name && hasPlatform {
+				isFound = true
+			}
+		}
+
+		if !isFound {
+			t.Errorf("Function %s not present in the output", expectedFunction.Name)
+		}
+	}
+}
+
+func getInputStoreFunctions(t *testing.T) []schema.StoreFunction {
+	inputJSONBytes := []byte(`[{
+        "title": "NodeInfo",
+        "name": "nodeinfo",
+        "description": "Get info about the machine that you're deployed on. Tells CPU count, hostname, OS, and Uptime",
+        "images": {
+            "arm64": "functions/nodeinfo:arm64",
+            "armhf": "functions/nodeinfo:latest-armhf",
+            "x86_64": "functions/nodeinfo:latest"
+        },
+        "fprocess": "node main.js",
+        "network": "func_functions",
+        "repo_url": "https://github.com/openfaas/faas/tree/master/sample-functions/NodeInfo"
+    },
+    {
+        "title": "sha512sum",
+        "name": "sha512sum",
+        "description": "Generate shasums in 512 format",
+        "images": {
+            "arm64": "functions/alpine:latest-arm64"
+        },
+        "fprocess": "sha512sum",
+        "network": "func_functions",
+        "repo_url": "https://github.com/openfaas/faas/tree/master/sample-functions/AlpineFunction"
+    },
+    {
+        "title": "Figlet",
+        "name": "figlet",
+        "description": "Generate ASCII logos with the figlet CLI",
+        "images": {
+			"armhf": "functions/figlet:latest-armhf",
+            "x86_64": "functions/figlet:0.9.6"
+        },
+        "fprocess": "figlet",
+        "repo_url": "https://github.com/openfaas/faas/tree/master/sample-functions/figlet"
+	}]`)
+
+	var inputFunctions []schema.StoreFunction
+	err := json.Unmarshal(inputJSONBytes, &inputFunctions)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	return inputFunctions
+}
+
+func Test_filterStoreList_x86_64(t *testing.T) {
+	outputJSONBytes := []byte(`[{
+        "title": "NodeInfo",
+        "name": "nodeinfo",
+        "description": "Get info about the machine that you're deployed on. Tells CPU count, hostname, OS, and Uptime",
+        "images": {
+            "arm64": "functions/nodeinfo:arm64",
+            "armhf": "functions/nodeinfo:latest-armhf",
+            "x86_64": "functions/nodeinfo:latest"
+        },
+        "fprocess": "node main.js",
+        "network": "func_functions",
+        "repo_url": "https://github.com/openfaas/faas/tree/master/sample-functions/NodeInfo"
+    }, {
+		"title": "Figlet",
+        "name": "figlet",
+        "description": "Generate ASCII logos with the figlet CLI",
+        "images": {
+            "armhf": "functions/figlet:latest-armhf",
+            "x86_64": "functions/figlet:0.9.6"
+        },
+        "fprocess": "figlet",
+        "repo_url": "https://github.com/openfaas/faas/tree/master/sample-functions/figlet"
+	}]`)
+
+	inputFunctions := getInputStoreFunctions(t)
+	var expectedOuputFunctions []schema.StoreFunction
+	err := json.Unmarshal(outputJSONBytes, &expectedOuputFunctions)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	filteredFunctions := filterStoreList(inputFunctions, "x86_64")
+	matchFilteredOutout(t, expectedOuputFunctions, filteredFunctions, "x86_64")
+
+}
+
+func Test_filterStoreList_armhf(t *testing.T) {
+	outputJSONBytes := []byte(`[{
+        "title": "NodeInfo",
+        "name": "nodeinfo",
+        "description": "Get info about the machine that you're deployed on. Tells CPU count, hostname, OS, and Uptime",
+        "images": {
+            "arm64": "functions/nodeinfo:arm64",
+            "armhf": "functions/nodeinfo:latest-armhf",
+            "x86_64": "functions/nodeinfo:latest"
+        },
+        "fprocess": "node main.js",
+        "network": "func_functions",
+        "repo_url": "https://github.com/openfaas/faas/tree/master/sample-functions/NodeInfo"
+    }, {
+		"title": "Figlet",
+        "name": "figlet",
+        "description": "Generate ASCII logos with the figlet CLI",
+        "images": {
+            "armhf": "functions/figlet:latest-armhf",
+            "x86_64": "functions/figlet:0.9.6"
+        },
+        "fprocess": "figlet",
+        "repo_url": "https://github.com/openfaas/faas/tree/master/sample-functions/figlet"
+	}]`)
+
+	inputFunctions := getInputStoreFunctions(t)
+	var expectedOuputFunctions []schema.StoreFunction
+	err := json.Unmarshal(outputJSONBytes, &expectedOuputFunctions)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	filteredFunctions := filterStoreList(inputFunctions, "armhf")
+	matchFilteredOutout(t, expectedOuputFunctions, filteredFunctions, "armhf")
+
+}
+
+func Test_filterStoreList_arm64(t *testing.T) {
+	outputJSONBytes := []byte(`[{
+        "title": "NodeInfo",
+        "name": "nodeinfo",
+        "description": "Get info about the machine that you're deployed on. Tells CPU count, hostname, OS, and Uptime",
+        "images": {
+            "arm64": "functions/nodeinfo:arm64",
+            "armhf": "functions/nodeinfo:latest-armhf",
+            "x86_64": "functions/nodeinfo:latest"
+        },
+        "fprocess": "node main.js",
+        "network": "func_functions",
+        "repo_url": "https://github.com/openfaas/faas/tree/master/sample-functions/NodeInfo"
+    },{
+        "title": "sha512sum",
+        "name": "sha512sum",
+        "description": "Generate shasums in 512 format",
+        "images": {
+            "arm64": "functions/alpine:latest-arm64"
+        },
+        "fprocess": "sha512sum",
+        "network": "func_functions",
+        "repo_url": "https://github.com/openfaas/faas/tree/master/sample-functions/AlpineFunction"
+    }]`)
+
+	inputFunctions := getInputStoreFunctions(t)
+	var expectedOuputFunctions []schema.StoreFunction
+	err := json.Unmarshal(outputJSONBytes, &expectedOuputFunctions)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	filteredFunctions := filterStoreList(inputFunctions, "arm64")
+	matchFilteredOutout(t, expectedOuputFunctions, filteredFunctions, "arm64")
+
+}
+
+func Test_filterStoreList_other(t *testing.T) {
+	outputJSONBytes := []byte(`[]`)
+
+	inputFunctions := getInputStoreFunctions(t)
+	var expectedOuputFunctions []schema.StoreFunction
+	err := json.Unmarshal(outputJSONBytes, &expectedOuputFunctions)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	filteredFunctions := filterStoreList(inputFunctions, "other")
+	matchFilteredOutout(t, expectedOuputFunctions, filteredFunctions, "other")
+
+}
