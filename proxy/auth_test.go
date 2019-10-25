e83806606e949c64d4950f6d1edd3507cc6a8b33
Vivek Singh <vivekkmr45@yahoo.in>
2019-10-25 12:45:02 +0530

    Update unit tests and review comments
    Signed-off-by: Vivek Singh <vivekkmr45@yahoo.in>

--- proxy/auth_test.go
+++ proxy/auth_test.go
@@ -47,3 +47,54 @@ func Test_SetAuth_SkipAuthorization(t *testing.T) {
 		t.Errorf("got header %q, want none", header)
 	}
 }
+
+func Test_AddAuth_Oauth2(t *testing.T) {
+	//setup store
+	config.DefaultDir, _ = ioutil.TempDir("", "faas-cli-auth-test")
+	config.DefaultFile = "authtest2.yml"
+	basicAuthURL := strings.TrimRight("http://openfaas.ouath.test/", "/")
+	token := "somebase64string"
+	config.UpdateAuthConfig(basicAuthURL, token, config.Oauth2AuthType)
+
+	req, _ := http.NewRequest("GET", basicAuthURL, nil)
+	AddAuth(req, basicAuthURL)
+	header := req.Header.Get("Authorization")
+	expectedValue := "Bearer " + token
+	if header != expectedValue {
+		t.Errorf("got header %q, want %q", header, expectedValue)
+	}
+}
+
+func Test_AddAuth_BasicAuth(t *testing.T) {
+	//setup store
+	config.DefaultDir, _ = ioutil.TempDir("", "faas-cli-auth-test")
+	config.DefaultFile = "authtest2.yml"
+	basicAuthURL := strings.TrimRight("http://openfaas.basic-auth.test/", "/")
+	token := config.EncodeAuth("username", "password")
+	config.UpdateAuthConfig(basicAuthURL, token, config.BasicAuthType)
+
+	req, _ := http.NewRequest("GET", basicAuthURL, nil)
+	AddAuth(req, basicAuthURL)
+	header := req.Header.Get("Authorization")
+	expectedValue := "Basic " + token
+	if header != expectedValue {
+		t.Errorf("got header %q, want %q", header, expectedValue)
+	}
+}
+
+func Test_AddAuth_SkipAuthorization(t *testing.T) {
+	//setup store
+	config.DefaultDir, _ = ioutil.TempDir("", "faas-cli-auth-test")
+	config.DefaultFile = "authtest2.yml"
+	basicAuthURL := strings.TrimRight("http://openfaas.test/", "/")
+	openURL := "http://openfaas.test2/"
+	token := config.EncodeAuth("Aladdin", "open sesame")
+	config.UpdateAuthConfig(basicAuthURL, token, config.Oauth2AuthType)
+
+	req, _ := http.NewRequest("GET", openURL, nil)
+	SetAuth(req, openURL)
+	header := req.Header.Get("Authorization")
+	if header != "" {
+		t.Errorf("got header %q, want none", header)
+	}
+}
