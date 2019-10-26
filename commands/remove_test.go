e255a72fef0e91c14f6a1e07944da6991c81388d
Rene Kaufmann <kaufmann.r@gmail.com>
2019-10-26 13:43:48 +0200

    Update remove command priority
    This change is needed so that when a function name is given
    for the remove command, it prioritises that instead of deleting
    all of the functions in the stack YAML file.
    
    This is how I tested it:
      - create a temporary stack YAML file
      - run the delete command with this yaml file and also specify a function to
        delete
      - check the output to see if the specified function has been deleted
        (and not the function from the stack YAML)
    
    Fixes: #280
    
    Signed-off-by: Rene Kaufmann <kaufmann.r@gmail.com>

--- commands/remove_test.go
+++ commands/remove_test.go
@@ -4,11 +4,25 @@
 package commands
 
 import (
+	"io/ioutil"
 	"net/http"
+	"os"
+	"strings"
 	"testing"
 
 	"github.com/openfaas/faas-cli/test"
 )
+
+const testStack = `
+provider:
+  name: faas
+  gateway: http://127.0.0.1:8080
+
+functions:
+  fn1:
+    lang: go
+    handler: ./fn1
+`
 
 func Test_remove(t *testing.T) {
 	s := test.MockHttpServer(t, []test.Request{
@@ -20,12 +34,35 @@ func Test_remove(t *testing.T) {
 	})
 	defer s.Close()
 
+	// create a yaml stack with the function 'fn1'
+	tmpfile, err := ioutil.TempFile("", "stack.*.yml")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(tmpfile.Name())
+	defer tmpfile.Close()
+
+	if _, err := tmpfile.Write([]byte(testStack)); err != nil {
+		tmpfile.Close()
+		t.Fatal(err)
+	}
+	if err := tmpfile.Close(); err != nil {
+		t.Fatal(err)
+	}
+
 	resetForTest()
 
+	// run delete with a yaml file and also specify a function to delete.
+	// the explicitly specified function should be preferred over the function from the yaml file.
 	faasCmd.SetArgs([]string{
 		"remove",
+		"--yaml=" + tmpfile.Name(),
 		"--gateway=" + s.URL,
 		"test-function",
 	})
-	faasCmd.Execute()
+	commandOutput := test.CaptureStdout(func() { faasCmd.Execute() })
+
+	if !strings.Contains(commandOutput, "Deleting: test-function") {
+		t.Error("test-function should be deleted.")
+	}
 }
