a8cbed3ba1e76c2598e3857b7e4b26e9308c4b9f
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2019-11-28 15:21:58 +0000

    Download kubeseal via faas-cli cloud seal command
    Closes: #722 and closes: #724 by @utsavanand2
    
    This is the combination of the approach taken by @utsavanand2 and
    the guidance offered by @rgee0 and @LucasRoesler combined into
    a single PR. It also uses the GitHub HTTP website instead of the
    GitHub API - the GitHub API has a rate-limit of 50 requests per
    hour and is likely to get exhausted quickly, so this approach
    removes the issue in #724.
    
    Given that there is only one executable needed at this stage, the
    download command idea was removed in favour of a simple flag. The
    flag will be updated in the README.md and documentation after
    the merge and release.
    
    Tested with --download and --download-version with an override
    on MacOS.
    
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- commands/cloud.go
+++ commands/cloud.go
@@ -8,21 +8,29 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"os"
 	"os/exec"
+	"path"
 	"path/filepath"
+	"runtime"
+	"strings"
+	"time"
 
+	"github.com/openfaas/faas-cli/proxy"
 	"github.com/openfaas/faas-cli/schema"
 	"github.com/spf13/cobra"
 )
 
 var (
-	namespace  string
-	name       string
-	literal    *[]string
-	outputFile string
-	fromFile   *[]string
-	certFile   string
+	namespace       string
+	name            string
+	literal         *[]string
+	outputFile      string
+	fromFile        *[]string
+	certFile        string
+	download        bool
+	downloadVersion string
 )
 
 func init() {
@@ -34,6 +42,9 @@ func init() {
 	cloudSealCmd.Flags().StringVarP(&certFile, "cert", "c", "pub-cert.pem", "Filename of public certificate")
 
 	cloudSealCmd.Flags().StringVarP(&outputFile, "output-file", "o", "secrets.yml", "Output file for secrets")
+
+	cloudSealCmd.Flags().BoolVar(&download, "download", false, "Download the kubeseal binary required for this command, see also --download-version")
+	cloudSealCmd.Flags().StringVar(&downloadVersion, "download-version", "", "Specifiy a kubeseal version to download")
 
 	literal = cloudSealCmd.Flags().StringArrayP("literal", "l", []string{}, "Secret literal key-value data")
 
@@ -49,15 +60,22 @@ var cloudCmd = &cobra.Command{
 }
 
 var cloudSealCmd = &cobra.Command{
-	Use:   `seal [--name secret-name] [--literal k=v] [--from-file] [--namespace openfaas-fn]`,
+	Use:   `seal [--name secret-name] [--literal k=v] [--from-file] [--namespace openfaas-fn] [--download]`,
 	Short: "Seal a secret for usage with OpenFaaS Cloud",
 	Example: `  faas-cli cloud seal --name alexellis-github --literal hmac-secret=c4488af0c158e8c
   faas-cli cloud seal --name alexellis-token --from-file api-key.txt
-  faas-cli cloud seal --name alexellis-token --literal a=b --literal c=d --cert pub-cert.pem`,
+  faas-cli cloud seal --name alexellis-token --literal a=b --literal c=d --cert pub-cert.pem
+  faas-cli cloud seal --download
+  faas-cli cloud seal --download --download-version v0.9.5`,
 	RunE: runCloudSeal,
 }
 
 func runCloudSeal(cmd *cobra.Command, args []string) error {
+
+	if download {
+		return downloadKubeSeal()
+	}
+
 	if len(name) == 0 {
 		return fmt.Errorf("--name is required")
 	}
@@ -132,4 +150,99 @@ func runCloudSeal(cmd *cobra.Command, args []string) error {
 
 	return nil
 
+}
+
+func downloadKubeSeal() error {
+	releases := "https://github.com/bitnami-labs/sealed-secrets/releases/latest"
+
+	releaseVersion := downloadVersion
+	if len(downloadVersion) == 0 {
+		version, err := findRelease(releases)
+		if err != nil {
+			return err
+		}
+		releaseVersion = version
+	}
+
+	osVal := runtime.GOOS
+	arch := runtime.GOARCH
+
+	if arch == "x86_64" {
+		arch = "amd64"
+	}
+
+	downloadURL := "https://github.com/bitnami/sealed-secrets/releases/download/" + releaseVersion + "/kubeseal-" + osVal + "-" + arch
+
+	fmt.Printf("Starting download of kubeseal %s, this could take a few moments.\n", releaseVersion)
+	output, err := downloadBinary(http.DefaultClient, downloadURL, "kubeseal")
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf(`Download completed, please run:
+
+  chmod +x %s
+  sudo install %s /usr/local/bin/
+  /usr/local/bin/kubeseal --version
+
+  `, output, output)
+
+	return nil
+}
+
+func findRelease(url string) (string, error) {
+	timeout := time.Second * 5
+	client := proxy.MakeHTTPClient(&timeout, false)
+	client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	}
+
+	req, err := http.NewRequest(http.MethodHead, url, nil)
+	if err != nil {
+		return "", err
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
+	if res.StatusCode != 302 {
+		return "", fmt.Errorf("incorrect status code: %d", res.StatusCode)
+	}
+
+	loc := res.Header.Get("Location")
+	if len(loc) == 0 {
+		return "", fmt.Errorf("unable to determine release of kubeseal")
+	}
+	version := loc[strings.LastIndex(loc, "/")+1:]
+	return version, nil
+}
+
+func downloadBinary(client *http.Client, url, name string) (string, error) {
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return "", err
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	tempDir := os.TempDir()
+	outputPath := path.Join(tempDir, name)
+	if res.Body != nil {
+		defer res.Body.Close()
+		res, _ := ioutil.ReadAll(res.Body)
+
+		err := ioutil.WriteFile(outputPath, res, 0600)
+		if err != nil {
+			return "", err
+		}
+		return outputPath, nil
+	}
+	return "", fmt.Errorf("error downloading %s", url)
 }
