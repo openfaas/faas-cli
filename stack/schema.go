4b62db380b15c8849f5445c3a944c3c9306c01ce
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2020-01-15 16:53:33 +0000

    Add ability to specify handler folder for template
    Fixes: #754
    
    * The destination folder for the user's function / handler was
    hard-coded to ./function/ or ./ for the Dockerfile template, this
    patch allows users to override the value in template.yml
    * Set handler_folder to a custom value such as "src"
    
    Tested by updating the node template to use a handler_folder of
    src, the build failed, then the Dockerfile was updated to match
    the src folder instead of function, this resolved the error
    and once deployed, it worked as expected.
    
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- stack/schema.go
+++ stack/schema.go
@@ -99,11 +99,13 @@ type Services struct {
 
 // LanguageTemplate read from template.yml within root of a language template folder
 type LanguageTemplate struct {
-	Language     string        `yaml:"language"`
-	FProcess     string        `yaml:"fprocess"`
-	BuildOptions []BuildOption `yaml:"build_options"`
+	Language     string        `yaml:"language,omitempty"`
+	FProcess     string        `yaml:"fprocess,omitempty"`
+	BuildOptions []BuildOption `yaml:"build_options,omitempty"`
 	// WelcomeMessage is printed to the user after generating a function
-	WelcomeMessage string `yaml:"welcome_message"`
+	WelcomeMessage string `yaml:"welcome_message,omitempty"`
+	// HandlerFolder to copy the function code into
+	HandlerFolder string `yaml:"handler_folder,omitempty"`
 }
 
 // BuildOption a named build option for one or more packages
