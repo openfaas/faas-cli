ca8b9df157d15fa73274593ac1b45e18d857c0dc
John McCabe <john@johnmccabe.net>
2020-02-03 23:06:03 +0000

    Up --watch functionality
    Signed-off-by: John McCabe <john@johnmccabe.net>

--- commands/up.go
+++ commands/up.go
@@ -5,7 +5,14 @@ package commands
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+	"time"
 
+	"github.com/fsnotify/fsnotify"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 )
@@ -13,6 +20,7 @@ import (
 var (
 	skipPush   bool
 	skipDeploy bool
+	watch    bool
 )
 
 func init() {
@@ -20,6 +28,8 @@ func init() {
 	upFlagset := pflag.NewFlagSet("up", pflag.ExitOnError)
 	upFlagset.BoolVar(&skipPush, "skip-push", false, "Skip pushing function to remote registry")
 	upFlagset.BoolVar(&skipDeploy, "skip-deploy", false, "Skip function deployment")
+	upFlagset.BoolVar(&watch, "watch", false, "Watch for filesystem changes")
+
 	upCmd.Flags().AddFlagSet(upFlagset)
 
 	build, _, _ := faasCmd.Find([]string{"build"})
@@ -64,6 +74,93 @@ func preRunUp(cmd *cobra.Command, args []string) error {
 }
 
 func upHandler(cmd *cobra.Command, args []string) error {
+
+	if watch {
+		watcher, err := fsnotify.NewWatcher()
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer watcher.Close()
+
+		done := make(chan bool)
+		go func() {
+			for {
+				select {
+				case event := <-watcher.Events:
+
+					log.Printf("event: %s", event)
+
+					if strings.HasSuffix(event.Name, "~") {
+						break
+					}
+
+					windowsBuildDir := "\\build"
+					// linuxBuildDir := "/build"
+
+					if strings.Contains(event.Name, windowsBuildDir) {
+						break
+					}
+					if event.Op&fsnotify.Write == fsnotify.Write {
+						log.Printf("WRITE detected, sleeping")
+						time.Sleep(100 * time.Millisecond)
+						if err := doUp(cmd, args); err != nil {
+							log.Printf("Error detecting change: %v", err)
+						}
+					}
+					if event.Op&fsnotify.Create == fsnotify.Create {
+						log.Printf("CREATE detected, sleeping")
+					}
+				case err := <-watcher.Errors:
+					fmt.Println("error:", err)
+				}
+			}
+		}()
+
+		cwd, err := os.Getwd()
+		if err != nil {
+			fmt.Println("error:", err)
+			return err
+		}
+		log.Println("Watching: ", cwd)
+
+		watchedDirs := []string{}
+		err = filepath.Walk(cwd,
+			func(path string, info os.FileInfo, err error) error {
+				if err != nil {
+					return err
+				}
+
+				if info.IsDir() && info.Name() == ".git" {
+					return filepath.SkipDir
+				}
+
+				if info.IsDir() {
+					fmt.Printf("watching: %s", path)
+					err = watcher.Add(path)
+					if err != nil {
+						log.Fatal(err)
+					}
+					watchedDirs = append(watchedDirs, path)
+				}
+				return nil
+			})
+		if err != nil {
+			log.Println(err)
+		}
+
+		<-done
+
+	} else {
+		doUp(cmd, args)
+	}
+	return nil
+}
+
+type runSingleton struct {
+	sync.Mutex
+}
+
+func doUp(cmd *cobra.Command, args []string) error {
 	if err := runBuild(cmd, args); err != nil {
 		return err
 	}
