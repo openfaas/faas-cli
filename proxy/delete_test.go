f9a4019176bf4ff0c2c3e7e39c8d2c01b3ddb94a
Itohan Ukponmwan <itohanukponmwan@gmail.com>
2020-04-19 17:45:26 -0700

    Remove returns non zero exit code for HTTP error
    This commit Fixes #748. It creates and returns an error when there is a
    HTTP error with the cli remove command and so a non zero exit code (1) is
    returned.
    
    Signed-off-by: Itohan Ukponmwan <itohanukponmwan@gmail.com>

--- proxy/delete_test.go
+++ proxy/delete_test.go
@@ -38,13 +38,11 @@ func Test_DeleteFunction_404(t *testing.T) {
 	cliAuth := NewTestAuth(nil)
 	proxyClient := NewClient(cliAuth, s.URL, nil, &defaultCommandTimeout)
 
-	stdout := test.CaptureStdout(func() {
-		proxyClient.DeleteFunction(context.Background(), "function-to-delete", "")
-	})
+	err := proxyClient.DeleteFunction(context.Background(), "function-to-delete", "")
 
 	r := regexp.MustCompile(`(?m:No existing function to remove)`)
-	if !r.MatchString(stdout) {
-		t.Fatalf("Want: %s, got: %s", "No existing function to remove", stdout)
+	if !r.MatchString(err.Error()) {
+		t.Fatalf("Want: %s, got: %s", "No existing function to remove", err.Error())
 	}
 }
 
@@ -55,12 +53,10 @@ func Test_DeleteFunction_Not2xxAnd404(t *testing.T) {
 	cliAuth := NewTestAuth(nil)
 	proxyClient := NewClient(cliAuth, s.URL, nil, &defaultCommandTimeout)
 
-	stdout := test.CaptureStdout(func() {
-		proxyClient.DeleteFunction(context.Background(), "function-to-delete", "")
-	})
+	err := proxyClient.DeleteFunction(context.Background(), "function-to-delete", "")
 
 	r := regexp.MustCompile(`(?m:Server returned unexpected status code)`)
-	if !r.MatchString(stdout) {
-		t.Fatalf("Output not matched: %s", stdout)
+	if !r.MatchString(err.Error()) {
+		t.Fatalf("Output not matched: %s", err.Error())
 	}
 }
