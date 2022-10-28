12e34f8689c3b870790b0174aed3ba84eeb185d9
Han Verstraete (OpenFaaS Ltd) <han@openfaas.com>
2022-10-28 18:00:42 +0200

    Switch to arkade archive package for reading tar files
    Signed-off-by: Han Verstraete (OpenFaaS Ltd) <han@openfaas.com>

--- commands/plugin_get.go
+++ commands/plugin_get.go
@@ -1,17 +1,14 @@
 package commands
 
 import (
-	"archive/tar"
 	"fmt"
-	"io"
-	"log"
 	"os"
 	"path"
-	"path/filepath"
 	"runtime"
 	"strings"
 	"time"
 
+	"github.com/alexellis/arkade/pkg/archive"
 	"github.com/alexellis/arkade/pkg/env"
 	"github.com/google/go-containerregistry/pkg/crane"
 	v1 "github.com/google/go-containerregistry/pkg/v1"
@@ -113,116 +110,12 @@ func runPluginGetCmd(cmd *cobra.Command, args []string) error {
 	}
 
 	defer os.Remove(tmpTar)
-	if err := untar(tarFile, pluginDir, true); err != nil {
+	gzipped := false
+	if err := archive.Untar(tarFile, pluginDir, gzipped, true); err != nil {
 		return fmt.Errorf("failed to untar %s: %w", tmpTar, err)
 	}
 	fmt.Printf("OK.. took: (%ds)\n", int(time.Since(st).Seconds()))
 	return nil
-}
-
-// TODO: switch to arkade package, but update it first so that it can take a gzip option true/false
-func untar(r io.Reader, dir string, quiet bool) (err error) {
-	t0 := time.Now()
-	nFiles := 0
-	madeDir := map[string]bool{}
-	defer func() {
-		td := time.Since(t0)
-
-		if err == nil {
-			if !quiet {
-				log.Printf("extracted tarball into %s: %d files, %d dirs (%v)", dir, nFiles, len(madeDir), td)
-			}
-		} else {
-			log.Printf("error extracting tarball into %s after %d files, %d dirs, %v: %v", dir, nFiles, len(madeDir), td, err)
-		}
-
-	}()
-
-	tr := tar.NewReader(r)
-	loggedChtimesError := false
-	for {
-		f, err := tr.Next()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Printf("tar reading error: %v", err)
-			return fmt.Errorf("tar error: %v", err)
-		}
-		if !validRelPath(f.Name) {
-			return fmt.Errorf("tar contained invalid name error %q", f.Name)
-		}
-		baseFile := filepath.Base(f.Name)
-		abs := path.Join(dir, baseFile)
-		if !quiet {
-			fmt.Printf("Extracting: %s to\t%s\n", f.Name, abs)
-		}
-
-		fi := f.FileInfo()
-		mode := fi.Mode()
-		switch {
-		case mode.IsDir():
-
-			break
-
-		case mode.IsRegular():
-
-			wf, err := os.OpenFile(abs, os.O_RDWR|os.O_CREATE|os.O_TRUNC, mode.Perm())
-			if err != nil {
-				return err
-			}
-			n, err := io.Copy(wf, tr)
-			if closeErr := wf.Close(); closeErr != nil && err == nil {
-				err = closeErr
-			}
-			if err != nil {
-				return fmt.Errorf("error writing to %s: %v", abs, err)
-			}
-			if n != f.Size {
-				return fmt.Errorf("only wrote %d bytes to %s; expected %d", n, abs, f.Size)
-			}
-			modTime := f.ModTime
-			if modTime.After(t0) {
-				// Clamp modtimes at system time. See
-				// golang.org/issue/19062 when clock on
-				// buildlet was behind the gitmirror server
-				// doing the git-archive.
-				modTime = t0
-			}
-			if !modTime.IsZero() {
-				if err := os.Chtimes(abs, modTime, modTime); err != nil && !loggedChtimesError {
-					// benign error. Gerrit doesn't even set the
-					// modtime in these, and we don't end up relying
-					// on it anywhere (the gomote push command relies
-					// on digests only), so this is a little pointless
-					// for now.
-					log.Printf("error changing modtime: %v (further Chtimes errors suppressed)", err)
-					loggedChtimesError = true // once is enough
-				}
-			}
-			nFiles++
-		default:
-		}
-	}
-	return nil
-}
-
-func validRelativeDir(dir string) bool {
-	if strings.Contains(dir, `\`) || path.IsAbs(dir) {
-		return false
-	}
-	dir = path.Clean(dir)
-	if strings.HasPrefix(dir, "../") || strings.HasSuffix(dir, "/..") || dir == ".." {
-		return false
-	}
-	return true
-}
-
-func validRelPath(p string) bool {
-	if p == "" || strings.Contains(p, `\`) || strings.HasPrefix(p, "/") || strings.Contains(p, "../") {
-		return false
-	}
-	return true
 }
 
 func getClientArch() (arch string, os string) {
