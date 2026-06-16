1722f102f92fa1687cd102596ba666d67b5f04df
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2026-06-16 15:28:38 +0100

    Remove legacy single-dash flag translation layer
    Drop translateLegacyOpts() and associated tests. The "-" prefix
    Go-style flags (-action, -image, -name, etc.) have been deprecated
    for years; users should use cobra verb + "--flag" syntax.
    
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- main.go
+++ main.go
@@ -4,17 +4,11 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/openfaas/faas-cli/commands"
 )
 
 func main() {
-	customArgs, err := translateLegacyOpts(os.Args)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err.Error())
-		os.Exit(1)
-	}
-	commands.Execute(customArgs)
+	commands.Execute(os.Args)
 }
